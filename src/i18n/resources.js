f240f2044cbc77ee0c99c09b9e00cc3184c4fa41
MohamedEssam1695 <toma126542@gmail.com>
2022-07-22 22:34:25 +0200

    feat: added app Theme component

--- src/i18n/resources.js
+++ src/i18n/resources.js
@@ -1,14 +1,20 @@
 import fallback from "./fallback";
 import feedback from "./feedback";
 
+import app from "../modules/App/locales";
 const resources = {
   ar: {
     fallback: fallback.ar,
     feedback: feedback.ar,
+
+    app: app.ar
   },
   en: {
     fallback: fallback.en,
     feedback: feedback.en,
+
+    app: app.en
+
   },
 };
 
---
+++ src/modules/App/locales/index.js
@@ -0,0 +1,6 @@
+import ar from "./ar";
+import en from "./en";
+
+const locales = { ar, en };
+
+export default locales;
