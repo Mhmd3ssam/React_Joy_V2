300b519042209aad219b9285b4249165b6bd8a46
MohamedEssam1695 <toma126542@gmail.com>
2022-07-22 20:40:17 +0200

    init: added roboto font to the app root

---
+++ src/constants/global.js
@@ -0,0 +1,6 @@
+export const LANGS = { en: "en", ar: "ar" };
+export const LANGS_DIRS = { en: "ltr", ar: "rtl" };
+export const LANGS_FONTS = { en: "Roboto", ar: "Tajawal" };
+export const I18N_MAP = { en: "en", ar: "ar" };
+
+export const CURRENCIES = { sar: "SAR" };
