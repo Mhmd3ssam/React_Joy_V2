ebb52679e74ea0db9d50224c0213c502d21a1aae
MohamedEssam1695 <toma126542@gmail.com>
2022-07-22 21:42:53 +0200

    feat: added icons constant file

---
+++ src/helpers/icons.js
@@ -0,0 +1,25 @@
+import {
+  mdiChartBar,
+  mdiOrderBoolAscendingVariant,
+  mdiSitemapOutline,
+  mdiAccountCircleOutline,
+  mdiFileDocumentOutline,
+  mdiSwapHorizontalBold,
+  mdiWalletOutline,
+  mdiTools,
+  mdiAccountSupervisorOutline,
+  mdiCogs,
+} from "@mdi/js";
+
+export const MODS_ICONS = {
+  dashboard: mdiChartBar,
+  orders: mdiOrderBoolAscendingVariant,
+  facilities: mdiSitemapOutline,
+  customers: mdiAccountCircleOutline,
+  invoices: mdiFileDocumentOutline,
+  transactions: mdiSwapHorizontalBold,
+  wallet: mdiWalletOutline,
+  services: mdiTools,
+  team: mdiAccountSupervisorOutline,
+  settings: mdiCogs,
+};
